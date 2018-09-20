5fa50ac5cf6585a89d4669a9ab179afbd58c587c
lakshaychhabra <lakshay.chhabra@hotmail.com>
2018-09-20 16:19:49 +0530

    isUnique-Array

--- Data Structure/ Arrays and Strings/isUnique/main.cpp
+++ Data Structure/ Arrays and Strings/isUnique/main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+// Implement an algorithm to determine if a string has all unique characters.
+
 int main() {
-    cout << "Hello, World!" << std::endl;
+
+    bool conditionArray[128]; // creating conditionArray boolean array which will work as condition checker.
+    string str;
+    getline(cin,str);
+
+    for(int i=0;i<str.length();i++){
+        if(conditionArray[str[i]]){
+            cout<<"Not Unique";
+            return false;
+        }
+
+        conditionArray[str[i]] = true;
+    }
+    cout<<"Unique";
+
     return 0;
 }
